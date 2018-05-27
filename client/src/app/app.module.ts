5306958c22b69fb49f86623ffac81c0a4a1286c9
willshepp28 <willsheppard28@outlook.com>
2018-05-26 22:57:48 -0400

    created login, home , signup components, and added routes

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,16 +1,43 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule, Http } from '@angular/http';
+import { Routes, RouterModule } from '@angular/router';
+
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
+
+import { AppRoutingModule }     from './app-routing.module';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LoginComponent,
+    SignupComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+
+/*
+
+  login
+  register
+  profile
+  profile/:id
+  activites/:id
+  
+*/
